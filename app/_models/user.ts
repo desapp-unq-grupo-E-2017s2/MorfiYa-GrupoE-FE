a981aae1766674c6251e6370e6434fd4908963ff
Alvaro <alvaropiorno100@gmail.com>
2017-11-11 15:47:13 -0300

    first commit

--- app/_models/user.ts
+++ app/_models/user.ts
@@ -1,7 +1,11 @@
 ﻿export class User {
     id: number;
-    username: string;
+    email: string;
     password: string;
     firstName: string;
     lastName: string;
+    cuit: string;
+    phone: string;
+    address: string;
+    location: string;
 }
